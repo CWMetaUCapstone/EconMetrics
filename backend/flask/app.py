d05896099e9bdb618dd6a70a90c67700e1415a1a
Carson Wolber <cwolber@meta.com>
2024-06-25 15:54:54 -0700

    removed default vite formatting and svgs

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,11 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
 if __name__ == '__main__':
     port = 3000
     print(f"Server is running on http://localhost:{port}") 
     app.run(port=port)
+
