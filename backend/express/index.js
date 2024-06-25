f3c0a57b04f4ff50ae4885a52a92e9442a28f5c0
Carson Wolber <cwolber@meta.com>
2024-06-25 13:42:08 -0700

    removed express, going for all flask backend

--- backend/express/index.js
+++
@@ -1,9 +0,0 @@
-express = require('express')
-const app = express()
-const PORT = 3000
-
-app.use(express.json())
-
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-}); 
