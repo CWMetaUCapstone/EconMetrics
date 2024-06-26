f8e24bba1655f3d1541b7df1eebd60894ab4606a
Carson Wolber <cwolber@meta.com>
2024-06-26 09:44:22 -0700

    established database connection through flask and postgres

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+load_dotenv()
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
-if __name__ == '__main__':
-    port = 3000
-    print(f"Server is running on http://localhost:{port}") 
-    app.run(port=port)
+def create_app():
+    database = SQLAlchemy(app)
+    return app, database
+
+if __name__ == '__main__': 
+    app, database = create_app()
+    with app.app_context():
+        database.create_all()
+        app.run(port=3000)
+
 
