664d435dfc5acfc4cc50fb0f348494b0de0ffddc
Carson Wolber <cwolber@meta.com>
2024-06-27 14:15:17 -0700

    added post request endpoint to flask and User class

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,19 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
-load_dotenv()
+from sqlalchemy import PickleType
+
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:blu3Song85@localhost:5432/econmetrics"
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    salary_range = db.Column(db.String(50))
+    location = db.Column(db.String(100))
+    roommates = db.Column(db.Integer)
+    children = db.Column(db.Integer)
+    job_title = db.Column(db.String(100))
+    transactions = db.Column(PickleType)
+
+
+@app.route('/profiles', methods=['POST'])
+def post_req_handler():
+    data = request.get_json()
+    request_obj = User(data=json.dumps(data))
+    db.session.add(request_obj)
+    db.session.commit()
+    return jsonify({'message': 'Data saved!'})
 
 def create_app():
-    database = SQLAlchemy(app)
-    return app, database
+    return app
 
-if __name__ == '__main__': 
-    app, database = create_app()
+if __name__ == '__main__':
+    app = create_app()
     with app.app_context():
-        database.create_all()
+        db.create_all()
         app.run(port=3000)
-
-
