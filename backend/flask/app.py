7a2edd09f47538cb3659ab3d1163dd447f83a6f0
Carson Wolber <cwolber@meta.com>
2024-06-28 00:22:25 -0700

    installed prisma

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import PickleType
 from flask_cors import CORS
+import json
 
 
 
@@ -9,20 +10,6 @@ app = Flask(__name__)
 CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:blu3Song85@localhost:5432/econmetrics"
 db = SQLAlchemy(app)
-
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    salary_range = db.Column(db.String(50))
-    location = db.Column(db.String(100))
-    roommates = db.Column(db.Integer)
-    children = db.Column(db.Integer)
-    job_title = db.Column(db.String(100))
-    transactions = db.Column(PickleType)
-
 
 @app.route('/profiles', methods=['POST'])
 def post_req_handler():
