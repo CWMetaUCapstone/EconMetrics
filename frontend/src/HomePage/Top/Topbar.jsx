60ad98d67231aa8ed6d091d578b06b2e15fd839b
Carson Wolber <cwolber@meta.com>
2024-06-28 13:03:24 -0700

    added home page routing to logo

--- frontend/src/HomePage/Top/Topbar.jsx
+++ frontend/src/HomePage/Top/Topbar.jsx
@@ -13,7 +13,7 @@ function Topbar() {
                     <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1z"/>
                 </svg>
             </div>
-            <p className='topname'>EconMetrics</p>
+            <p className='topname' onClick={() => navigate('/home')}>EconMetrics</p>
             <Search/>
 
             <button className='signinbtn'>Sign In</button>
