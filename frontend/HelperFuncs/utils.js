e73d642d4d14c02653e9e6c4b333d3bb216ae803
Carson Wolber <cwolber@meta.com>
2024-07-12 09:37:13 -0700

    added isValidPassword to resolve conflict

--- frontend/HelperFuncs/utils.js
+++ frontend/HelperFuncs/utils.js
@@ -162,8 +162,8 @@ export const getRows = (transactions) => {
     return rows;
 };
 
-
 /*
+<<<<<<< usersearch
 helper function to convert the dictionary form of a search object selected by a user into a 
 readable URL endpoint for navigaion. Each category adheres to the following unique form factors: 
 city: 'Menlo Park, CA' -> 'menloparkCA'
@@ -187,4 +187,54 @@ export function searchRouteFormatter(search) {
         let cleanedSearch = search.label.replace(/\s/g,'')
         return cleanedSearch
     }
+}
+
+
+/*
+helper function to check if a user's password meets the requirements of containing a special character, 
+upper and lower case letters, number, and length ≥ 8. If password passes all of these, return true and '' for the error message
+if false, return false and the appropriate error message. Returns are structured as a dictionary with fields [valid] of type
+bool to indicate password validity and [message] of type String for the error message. 
+*/
+export function isValidPassword(password){
+    let result = {
+        valid: true,
+        message: ''
+    }
+
+    const specialChars= /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    const upper = /[A-Z]/;
+    const lower = /[a-z]/;
+    const nums = /[0-9]/;
+
+    if(!specialChars.test([password])){
+        result.message = 'password must have at least 1 special character'
+        result.valid = false
+        return result
+    }
+
+    if(!upper.test(password)){
+        result.message = 'password must contain at least 1 upper case letter'
+        result.valid= false
+        return result
+    }
+
+    if(!lower.test(password)){
+        result.message = 'password must contain at least 1 lower case letter'
+        result.valid = false
+        return result
+    }
+
+    if(!nums.test([password])){
+        result.message = 'password must contain at least 1 number'
+        result.valid = false
+        return result
+    }
+
+    if(password.length < 8){
+        result.message = 'password must contain at least 8 characters'
+        result.valid = false
+        return result
+    }
+    return result
 }
